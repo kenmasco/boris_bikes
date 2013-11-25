2cf4161a4c2e5485dce51ee916f596491c7f70a4
Kenndy <kennerdeigh@me.com>
2013-11-25 15:09:44 +0000

    Added full? method, and capacity

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,8 +2,8 @@ require_relative "../lib/docking_station"
 
 describe DockingStation do
 
-  let (:bike) { Bike.new }
-  let (:station) { DockingStation.new }
+  let(:bike) { Bike.new }
+  let(:station) { DockingStation.new(:capacity => 20) }
 
   it "should accept a bike" do
     #we expect the station to have 0 bikes
@@ -14,10 +14,16 @@ describe DockingStation do
     expect(station.bike_count).to eq(1)
   end
 
-  it "should relese a bike" do
+  it "should release a bike" do
     station.dock(bike)
     station.release(bike)
     expect(station.bike_count).to eq (0)
+  end
+
+  it "should know when it's full" do
+    expect(station).not_to be_full
+    20.times { station.dock(Bike.new) }
+    expect(station).to be_full
   end
 
 end
