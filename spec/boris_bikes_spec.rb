ea057e8e469dce706e4ac4a2a2a99863f38dff89
Kenndy <kennerdeigh@me.com>
2013-11-25 13:11:12 +0000

    added new tests and methods to clear them, further defined Bike class

--- spec/boris_bikes_spec.rb
+++ spec/boris_bikes_spec.rb
@@ -7,5 +7,12 @@ describe Bike do
   it "should not be broken after we create it" do
     bike = Bike.new #initialize a new object of Bike class
     expect(bike).not_to be_broken
-  end 
+  end
+
+  it "should be able to break" do
+    bike = Bike.new
+    bike.break
+    expect(bike).to be_broken
+  end
+
 end
